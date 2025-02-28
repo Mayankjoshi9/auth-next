c3615bb7625db675a2cbd69d77bf9e1df93afa6b
mayankjoshi <mayankmonu9182j@gmail.com>
2025-02-28 23:28:03 +0530

    Refactor authSlice to enhance user type definition and improve state management

--- reducer/Slices/authSlice.ts
+++ reducer/Slices/authSlice.ts
@@ -1,24 +1,30 @@
-
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface AuthState {
-  user: string | null;
-  token: string | null;
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  username:string,
+  birthday:Date,
+  description:string,
+  gender:string,
+}
 
+interface AuthState {
+  user: User | null;
+  token: string | null;
 }
 
 const initialState: AuthState = {
-  user: typeof window !== "undefined" && localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") as string) : null, 
-  token: typeof window !== "undefined" && localStorage.getItem("token") ? localStorage.getItem("token"):null, 
-  
+  user: typeof window !== "undefined" && localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") as string) : null,
+  token: typeof window !== "undefined" && localStorage.getItem("token") ? localStorage.getItem("token") : null,
 };
-
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<string | null>) => {
+    setUser: (state, action: PayloadAction<User>) => {
       state.user = action.payload;
     },
     setToken: (state, action: PayloadAction<string | null>) => {
